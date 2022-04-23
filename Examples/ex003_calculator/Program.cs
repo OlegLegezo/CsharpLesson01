b97570cfc219fbc758538ebad2f402a7892b5145
OlegLegezo <oleglegezo@yandex.ru>
2022-04-23 14:21:28 +0300

    ввод случайных чисел обновлен

--- Examples/ex003_calculator/Program.cs
+++ Examples/ex003_calculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿int A = new Random().Next(1,10);//вводим случайные числа в пример
-int B=2;
+Console.WriteLine(A);
+int B = new Random().Next(1,10);//вводим случайные числа в пример
+Console.WriteLine(B);
 int result=A+B;
 Console.WriteLine(result);
