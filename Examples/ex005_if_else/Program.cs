056af77393278905e511ff8db1066d7d2fd06d74
OlegLegezo <oleglegezo@yandex.ru>
2022-04-23 14:46:27 +0300

    if else пример с Эля эля

---
+++ Examples/ex005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "эля")
+{
+    Console.WriteLine("привет, Элечка!!!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
